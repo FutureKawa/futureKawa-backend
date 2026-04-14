524fe7d8fd135df13ec4cd296766e7feb7c8e83e
MattheoC-code <mattheo.coppin@ecoles-epsi.net>
2026-04-14 14:16:22 +0200

    init backend fk

---
+++ src/main/java/com/futurekawa/backend_futurekawa/BackendFuturekawaApplication.java
@@ -0,0 +1,13 @@
+package com.futurekawa.backend_futurekawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFuturekawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFuturekawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futurekawa/backend_futurekawa/BackendFuturekawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurekawa.backend_futurekawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFuturekawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
